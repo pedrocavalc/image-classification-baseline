a0c5aa16088adbbabf18ed3e1fa8789aa5c2fc1f
pedrocavalc <pedro.cavalvante@lapisco.ifce.edu.br>
2023-02-18 14:21:00 -0300

    Implemeted kaagle dataset prototype

--- utils/fetch_database.py
+++ utils/fetch_database.py
@@ -4,6 +4,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import opendatasets as od
 import shutil
+import logging
 
 class DataSet():
     '''
@@ -22,22 +23,25 @@ class DataSet():
         '''
         Function to download the dataset
         '''
-        od.download(self.url)
+        if os.path.exists(os.path.join(self.data_dir,self.url.split('/')[-1])):
+            print(f"Found a existing folder for this dataset in directory '{self.data_dir}")
+        else:
+            od.download(self.url)
+            self.save_dataset()
 
     def save_dataset(self):
         if not os.path.exists(self.data_dir):
             os.mkdir(self.data_dir)
         path_dataset = self.url.split('/')[-1]
+
         path_dataset = os.path.join(path_dataset, os.listdir(path_dataset)[0])
-        shutil.move(path_dataset,self.data_dir)
-        print(f'Dataset has been downloaded to directory {self.data_dir}')
-        os.removedirs(self.url.split('/')[-1])
+        try:
+            shutil.move(path_dataset,self.data_dir)
+            print(f'Dataset has been downloaded to directory {self.data_dir}')
+            os.removedirs(self.url.split('/')[-1])
+        except:
+            print("Dataset has already been downloaded")
+            shutil.rmtree(self.url.split('/')[-1])
+            
 
-def main():
-    dataset = DataSet(url="https://www.kaggle.com/datasets/akhiljethwa/blood-cancer-image-dataset")
-    dataset.fech_data()
-    dataset.save_dataset()
 
-
-if __name__ == '__main__':
-    main()
