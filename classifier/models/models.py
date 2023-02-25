dc6bd3aa675ce07b6984e0b4ddb7874bcaae771e
pedrocavalc <pedro.cavalvante@lapisco.ifce.edu.br>
2023-02-25 13:37:52 -0300

    #17 implementado modelo Lenet

--- classifier/models/models.py
+++ classifier/models/models.py
@@ -33,7 +33,7 @@ class ResNet12(LightningModule):
         ImageClassificationBase (_Class_): Base class with the methods to evaluation the model
     """
     
-    def __init__(self,in_channels,num_classes):
+    def __init__(self,in_channels,num_classes,learning_rate):
         """
         Constructor of the class
 
@@ -43,6 +43,7 @@ class ResNet12(LightningModule):
         """
         super().__init__()
         self.num_classes = num_classes
+        self.lr = learning_rate
         # 3 x 224 x 224
         self.conv1= conv_block(in_channels,16) # 16 x 224 x 224
         self.conv2 = conv_block(16,32,pool=True) # 32 x 112 x 112
@@ -124,7 +125,93 @@ class ResNet12(LightningModule):
         return {'val_loss':loss.detach(), 'val_acc': acc}
     
     def configure_optimizers(self):
-        return torch.optim.Adam(self.parameters(), lr=0.02)
+        return torch.optim.Adam(self.parameters(), lr=self.lr)
     
     
 
+class LeNet(LightningModule):
+    
+    def __init__(self, in_channels, n_classes,learning_rate):
+        super().__init__()
+        self.num_classes = n_classes
+        self.lr = learning_rate
+        # fist conv
+        self.conv1 = conv_block(in_channels,16) # 16 x 224 x 224
+        self.conv2 = conv_block(16,32, pool=True) # 64 x 112 x  112
+        # second conv
+        self.conv3 = conv_block(32,64, pool=True) # 128 x 56 x 56
+        self.conv4 = conv_block(64,128, pool= True) # 192 x 28 x 28
+        self.conv5 = conv_block(128,192, pool=True) # 256 x 14 x 14
+        self.conv6 = conv_block(192, 256, pool= True) # 256 x 6 x 6
+        # fist and only set of FC
+        self.classifier = nn.Sequential(nn.MaxPool2d(2), # 256 x 3 x3
+        nn.Flatten(),     
+        nn.Linear(in_features= 256 * 3 * 3, out_features= 256 *3 *3 ), # 256 x 3 x 3
+                    nn.ReLU(),
+                    nn.Linear(in_features = 256 * 3 * 3, out_features = n_classes),
+                    nn.LogSoftmax(dim=1)
+                    )
+
+    
+        
+    def forward(self,xb):
+        """
+        foward method to pass the data network throug architeture 
+
+        Returns:
+            _list_: the output predictions
+        """
+        out = self.conv1(xb)
+        out = self.conv2(out)
+        out = self.conv3(out)
+        
+        out = self.conv4(out)
+        out = self.conv5(out)
+        out = self.conv6(out)
+        
+        out = self.classifier(out)
+        
+        return out
+    
+    def training_step(self, batch):
+        """
+        Method to get the loss of the training step of the model
+
+        Args:
+            batch (_DataLoader object_): batch of images that will be used in training step
+
+        Returns:
+            _float_: returns the loss of step
+        """
+        images, labels = batch
+        out = self(images)
+        loss = F.cross_entropy(out,labels)
+        self.log("train_loss",loss, on_epoch=True)
+
+    def validation_step(self, batch, batch_idx):
+        """
+        Method to get the validation loss and validation accuracy for the step of the model
+
+        Args:
+            batch (_DataLoader object_): batch of images that will be used in training step
+
+        Returns:
+            _dict_: returns a dict with the validation loss and validation accuracy of the step 
+        """
+        images, labels = batch
+        
+        out = self(images)
+        
+        loss = F.cross_entropy(out, labels)
+        if self.num_classes == 2:
+            acc = accuracy(out, labels,task='binary')
+        else:
+            acc = accuracy(out,labels, task='multiclass',num_classes=self.num_classes)
+        
+        self.log("val_loss",loss, on_epoch=True)
+        self.log("acc", acc, on_epoch=True)
+        
+        return {'val_loss':loss.detach(), 'val_acc': acc}
+    
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr = self.lr)
