4ca45f77c2182af313cae81efee8df7750b54db6
pedrocavalc <pedro.cavalvante@lapisco.ifce.edu.br>
2023-03-15 13:33:58 -0300

    #22 DenseNet implemented

--- classifier/models/models_config.py
+++ classifier/models/models_config.py
@@ -1,11 +1,15 @@
-from classifier.models.models import ResNet12, LeNet
+from classifier.models.models import *
 
 
 
 def config_models(num_classes,lr):
 
-    models_configs = {'res_net12':ResNet12(3,num_classes,lr), 
-                      'LeNet':LeNet(3, num_classes,lr)
+    models_configs = {
+                      #'res_net12':ResNet12(3,num_classes,lr), 
+                      #'LeNet':LeNet(3, num_classes,lr),
+                      #'AlexNet': AlexNet(3, num_classes,lr),
+                      'DenseNet': DenseNet(growthRate=12, depth=100, reduction=0.5,
+                            bottleneck=True, nClasses=num_classes,lr = lr)
                       }
     
     return models_configs
