12ef9498ffc0e6f67e556523b61555a675994dde
pedrocavalc <pedro.cavalvante@lapisco.ifce.edu.br>
2023-03-15 10:33:44 -0300

    #22 implemented AlexNet architecture

--- classifier/models/models.py
+++ classifier/models/models.py
@@ -31,13 +31,14 @@ def conv_block(in_channels, out_channels, pool = False):
 
 class ResNet12(BaseModel,LightningModule):
     """
-    Class to implement ResNet12 architecture, extends from ImageClassificationBase class that is reponsible to implemente the basics
+    Class to implement ResNet12 architecture, extends from BaseModel class that is reponsible for implement the basics
     model evaluations methods.
     More details of ResNet12 architecture can be found on:
     https://www.researchgate.net/figure/The-structure-of-ResNet-12_fig1_329954455
 
     Args:
-        ImageClassificationBase (_Class_): Base class with the methods to evaluation the model
+        BaseModel (Class): Class that is reponsible for implement the basics model evaluations methods
+        LightningModule (Class): Class from pytorch-ligthining that have abstractions to handle with metrics and use auto learning rate finder
     """
     def __init__(self,in_channels,num_classes,learning_rate):
         """
@@ -95,6 +96,15 @@ class ResNet12(BaseModel,LightningModule):
     
 
 class LeNet(BaseModel,LightningModule):
+    """
+    Class to implement LeNet12 architecture, extends from BaseModel class that is reponsible for implement the basics
+    model evaluations methods.
+    More details of LeNet12 architecture can be found on:
+
+    Args:
+        BaseModel (Class): Class that is reponsible for implement the basics model evaluations methods
+        LightningModule (Class): Class from pytorch-ligthining that have abstractions to handle with metrics and use auto learning rate finder
+    """
     def __init__(self, in_channels, n_classes,learning_rate):
         super().__init__()
         self.num_classes = n_classes
@@ -138,3 +148,89 @@ class LeNet(BaseModel,LightningModule):
     
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr = (self.lr or self.learning_rate))
+    
+    
+
+
+
+class AlexNet(BaseModel,LightningModule):
+    """
+    Class to implement AlexNet architecture, extends from BaseModel , and LighthingModule.
+    More details of AlexNet architecture can be found on:
+    https://arxiv.org/abs/1803.01164
+    
+
+    Args:
+        BaseModel (Class): Class that is reponsible for implement the basics model evaluations methods
+        LightningModule (Class): Class from pytorch-ligthining that have abstractions to handle with metrics and use auto learning rate finder
+    """
+    def __init__(self, in_channels, n_classes,learning_rate):
+        super().__init__()
+        self.num_classes = n_classes
+        self.lr = learning_rate
+        
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(in_channels, 96, kernel_size=11, stride=4, padding=0),
+            nn.BatchNorm2d(96),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size = 3, stride = 2)
+                                    )
+        
+        self.layer2 = nn.Sequential(nn.Conv2d(96, 256, kernel_size=5, stride=1, padding=2),
+                                    nn.BatchNorm2d(256),
+                                    nn.ReLU(),
+                                    nn.MaxPool2d(kernel_size=3, stride=2)
+                                    )
+        
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(384),
+            nn.ReLU()
+        )
+        
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(384,384, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(384),
+            nn.ReLU()
+        )
+        
+        self.layer5 = nn.Sequential(
+            nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2)
+        )
+        
+        self.fc = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(9216, 4096),
+            nn.ReLU()
+        )
+        
+        self.fc1 = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(4096,4096),
+            nn.ReLU()
+        )
+        
+        self.fc2 = nn.Sequential(
+            nn.Linear(4096, n_classes)
+        )
+        
+    def forward(self, xb):
+        out = self.layer1(xb)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.layer5(out)
+        out = out.reshape(out.size(0), -1)
+        out = self.fc(out)
+        out = self.fc1(out)
+        out = self.fc2(out)
+        return out
+    
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr = (self.lr or self.learning_rate))
+    
+
+
